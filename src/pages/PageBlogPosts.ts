5b1630056b6c8ccdfc154f14da6bb07ad768e313
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-10 07:34:06 +0200

    showing blog posts without component

--- src/pages/PageBlogPosts.ts
+++ src/pages/PageBlogPosts.ts
@@ -1,7 +1,20 @@
+import blogPosts from '../data/blogPosts.json';
+import './styles/pageBlogPosts.scss';
+
 export const PageBlogPosts = () => {
 	return /*html*/`
 <div class="page pageBlogPosts">
-	<p>This is the blog posts page.</p>
+<p>There are ${blogPosts.length} blog posts.</p>
+
+<div class="blogPosts">
+	${blogPosts.map(blogPost => {
+			return `
+	<div class="blogPost">
+		${blogPost.title}	
+	</div>
+		`;
+		}).join('')}
+	</div>
 </div>
-`; 
+`;
 }
